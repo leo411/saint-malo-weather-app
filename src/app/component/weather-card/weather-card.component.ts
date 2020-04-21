a8e916b29c0ee66317347e39d83432b823b0aa5a
Leopoldine Hallynck <lhallynck@gmail.com>
2020-04-21 17:10:16 +0200

    forecasts/selectedForCast

--- src/app/component/weather-card/weather-card.component.ts
+++ src/app/component/weather-card/weather-card.component.ts
@@ -9,10 +9,14 @@ import { foreCasts } from 'src/mock-forecast';
 })
 export class WeatherCardComponent implements OnInit {
   foreCasts: WeatherPoint[] = foreCasts
+  selectedForeCast: WeatherPoint
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(foreCast: WeatherPoint): void {
+    this.selectedForeCast = foreCast
+  }
 }
